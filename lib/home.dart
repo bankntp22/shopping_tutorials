9d2c43bf6b3b32200598b0144bf16e6e650ffcf1
bank221 <bank.ntp22@gmail.com>
2022-11-26 21:12:01 +0700

    up

--- lib/home.dart
+++ lib/home.dart
@@ -17,48 +17,43 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   int _currentIndex = 0;
 
-  static  List<Widget>  listMenu = [ 
-    ScreenHome(),
-    ScreenOrder(),
-    ScreenAccount()
-  ];
+  static List<Widget> listMenu = [ScreenHome(), ScreenOrder(), ScreenAccount()];
 
-  void _updatedIndex (int value){
+  void _updatedIndex(int value) {
     setState(() {
       _currentIndex = value;
     });
   }
-  
-  
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-            backgroundColor: Colors.white70,
-            // appBar: AppBar(
-            //   title: Text('หมวดหมู่อาหาร'),
-            //   elevation: 1,
-            //   centerTitle: true,
-            //   leading: Icon(Icons.person),
-            //   backgroundColor: Colors.transparent,
-            //   actions: [
-            //     IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart))
-            //   ],
-            // ),
-            body: listMenu.elementAt(_currentIndex),
-            bottomNavigationBar: BottomNavigationBar(
-              selectedItemColor: Colors.amber[800],
-              backgroundColor: Colors.white,
-              onTap: _updatedIndex,
-              currentIndex: _currentIndex,
-              
-              items: [
-                BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
-                BottomNavigationBarItem(icon: Icon(Icons.settings),label: 'ORDER'),
-                BottomNavigationBarItem(icon: Icon(Icons.account_balance_wallet),label: 'Accounting'),
-              ],
-            ),
-            ));
+      backgroundColor: Colors.white70,
+      // appBar: AppBar(
+      //   title: Text('หมวดหมู่อาหาร'),
+      //   elevation: 1,
+      //   centerTitle: true,
+      //   leading: Icon(Icons.person),
+      //   backgroundColor: Colors.transparent,
+      //   actions: [
+      //     IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart))
+      //   ],
+      // ),
+      body: listMenu.elementAt(_currentIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.amber[800],
+        backgroundColor: Colors.white,
+        onTap: _updatedIndex,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'ORDER'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_balance_wallet), label: 'Accounting'),
+        ],
+      ),
+    ));
   }
 }
 
