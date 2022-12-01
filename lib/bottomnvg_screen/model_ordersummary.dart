2b999a0c9ecd739d85297ea0896cc026cab1c5bf
bank0220 <bank.ntp22@gmail.com>
2022-12-01 17:35:22 +0700

    up

--- lib/bottomnvg_screen/model_ordersummary.dart
+++ lib/bottomnvg_screen/model_ordersummary.dart
@@ -1,13 +1,7 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+
 class OrderSummary {
-  String sPayment;
-  String sTotal;
-
-  OrderSummary({
-    required this.sPayment,
-    required this.sTotal,
-  });
-
+   String? sPayment;
+   String? sTotal;
 }
 
 
